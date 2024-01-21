382924aa84eb39c46f41027525365e785cf3a87b
Fatih Ünal <93606693+uelca@users.noreply.github.com>
2024-01-21 14:53:20 +0100

    -

--- transcribeAudio.py
+++ transcribeAudio.py
@@ -4,9 +4,8 @@ import whisper
 model = whisper.load_model("large")
 
 # Transkribiere die Audiodatei
-result = model.transcribe("Interview Amina Souag.mp3", word_timestamps=True)
+result = model.transcribe("Pfad zum Interview", word_timestamps=True)
 
 # Speicher als .txt file
 with open("transciption.txt", "w") as f:
     f.write(result["text"])
-
